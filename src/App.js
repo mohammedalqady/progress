8b9c7e8c9692b78bfb2e1f80591eb086e8724734
mohammad <mohammad@gmail.com>
2024-10-28 09:32:33 +0100

    start app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+import "./index.css";
+import Register from "./page/Register";
+
+function App() {
+  return (
+    <Container>
+      <Register />
+    </Container>
+  );
+}
+
+export default App;
